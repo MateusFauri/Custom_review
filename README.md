36b7a87e7d9c572f4ebbe9b8a1c5ff407f91163e
Mateus Fauri <faurimateus@gmail.com>
2022-11-17 17:20:23 -0300

    Upload initial project

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # Custom_review
-Review a product's rating on amazon
+Review of an amazon store product using webscrapping to collect data and machine learning to analyze that data.
+
+## Table of Contents
+<p align="center">
+    1. [Status](#Status)
+    2. [Features](#Features)
+    3. [Prerequisite](#Prerequisite)
+    4. [Run](#Run)
+    5. [Technologies](#Technologies)
+    6. [Author](#Author)
+</p>
+
+### Status
+
+<h4 align="center"> 
+	🚧  In progress...  🚧
+</h4>
+
+### Features
+
+- [x] Collect Data from an amazon product
+- [ ] Create a sentiment analyzer for the data
+- [ ] Create charts to analyze data
+- [ ] The user enters the URL of an amazon produt
+
+### Prerequisite
+
+Before strarting, you will need to have the followings tools installed:
+
+[Git](https://git-scm.com), [python](https://www.python.org/).
+
+Not required, but advisable:
+
+venv, pyenv
+
+
+### Run
+
+```bash
+# Clone this repository
+$ git clone <git@github.com:MateusFauri/Custom_review.git>
+
+# Access project folder in terminal/cmd
+# If you are using venv, use:
+$ python -m venv some_folder_name
+
+# After activate your venv, install selenium using:
+$ pip install selenium
+
+# Just run:
+$ python main.py
+
+### Technologies
+
+The following tools were used in building the project:
+
+- Pyenv
+- Python
+- Selenium
+
+
+### Author
+---
+
+<a href="https://github.com/MateusFauri">
+ <img style="border-radius: 50%;" src="https://avatars.githubusercontent.com/u/62532715?v=4" width="100px;" alt=""/>
+ <br />
+ <sub><b>Thiago Marinho</b></sub></a> 
+
+
+By Mateus Fauri 👋🏽 
---
+++ main.py
@@ -0,0 +1,64 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.support import expected_conditions as EC
+
+URL = "https://www.amazon.com.br/Labirinto-Cubo-Gatomoderno-para-Gatos/product-reviews/B07YVM3SK1/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
+SLEEP_TIME = 2
+DRIVER_TIME = 10
+
+def checking_next(driver):
+    Xpath = "//ul[@class='a-pagination']/li"
+    wait = WebDriverWait(driver,DRIVER_TIME)
+    try:
+        wait.until(driver.find_elements(By.XPATH, Xpath ))
+    except:
+        time.sleep(SLEEP_TIME)
+        try:
+            wait.until(driver.find_elements(By.XPATH, Xpath ))
+        except:
+            return False
+    finally:
+        next = driver.find_elements(By.XPATH, Xpath)
+        print(next[1].get_attribute('class').find("a-disabled"))
+        if next[1].get_attribute('class').find("a-disabled") == -1:
+            return True
+        return False
+
+driver = webdriver.Chrome()
+driver.get(URL)
+
+with open('comments.txt', 'w', encoding="utf-8") as f:
+
+    try:
+        next_xpath = "//li[@class='a-last']/a[contains(text(), 'Próximo')]"
+        comment_xpath = "//div[@class='a-row a-spacing-small review-data']/span"
+        wait = WebDriverWait(driver, DRIVER_TIME)
+
+        while True:
+            time.sleep(SLEEP_TIME)
+            
+            try:
+                list_comment =  wait.until(lambda d: d.find_elements(By.XPATH, comment_xpath))
+            except StaleElementReferenceException:
+                time.sleep(SLEEP_TIME)
+                list_comment =  wait.until(lambda d: d.find_elements(By.XPATH, comment_xpath))
+
+            for comment in list_comment:
+                f.write(comment.text + "\n")
+
+            if checking_next(driver):
+                succeed = False
+                while not succeed:
+                    try:
+                        wait.until(EC.presence_of_element_located((By.XPATH, next_xpath))).click()
+                        succeed = True
+                    except:
+                        pass
+            else:
+                break
+    finally:
+        driver.quit()
+
